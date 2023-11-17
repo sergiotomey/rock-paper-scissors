6f9f628aaba714d93fa4bb8eec564e7c7833d9f6
SergioPC <sergiotomey96@gmail.com>
2023-11-17 17:12:52 -0300

    added ui using the dom

--- app.js
+++ app.js
@@ -1,35 +1,98 @@
+let playerScore = 0
+let CompScore = 0
+let playerScoreBox = document.querySelector('.playerScore')
+let compScoreBox = document.querySelector('.compScore')
+playerScoreBox.value = playerScore
+compScoreBox.value = CompScore
+let rockButton = document.querySelector(".rock-button")
+let paperButton = document.querySelector(".paper-button")
+let scissorsButton = document.querySelector(".scissors-button")
+let playButton = document.querySelector('.play-button')
+let playerAnounce = document.querySelector(".player-selection-anouncement")
+let playerSelection = document.createElement('h3')
+let finalResults = document.querySelector('.final-results')
+let userChoice
 
-function playGame(){
-    
-    let userChoice = prompt("Please choose Rock, Paper, or Scissors").toLowerCase()
+const rockSelection = () => {
+    playerSelection.textContent = 'You have chosen Rock!'
+    playerAnounce.appendChild(playerSelection)
+    return userChoice = 'rock'
 
-    if(userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'){
-        return 'please type rock, paper or scissors and try again'
-    }
+}
+const paperSelection = () => {
+    playerSelection.textContent = 'You have chosen Paper!'
+    playerAnounce.appendChild(playerSelection)
+    return userChoice = 'paper'
+}
+const scissorsSelection = () => {
+    playerSelection.textContent = 'You have chosen Scissors!'
+    playerAnounce.appendChild(playerSelection)
+    return userChoice = 'scissors'
+}
 
-    let computerChoice = Math.floor(Math.random() * (4-1)) + 1
 
-    if(computerChoice === 1){
+
+rockButton.addEventListener('click', rockSelection)
+paperButton.addEventListener('click', paperSelection)
+scissorsButton.addEventListener('click', scissorsSelection)
+
+
+
+
+function playGame() {
+
+    let computerChoice = Math.floor(Math.random() * (4 - 1)) + 1
+
+    if (computerChoice === 1) {
         computerChoice = 'rock'
-    }else if(computerChoice === 2){
+    } else if (computerChoice === 2) {
         computerChoice = 'paper'
-    }else if(computerChoice === 3){
+    } else if (computerChoice === 3) {
         computerChoice = 'scissors'
     }
 
-    if(userChoice === computerChoice){
-        return `Your ${userChoice} draws with computers ${computerChoice}.`
-    }else if(computerChoice === 'rock' && userChoice === 'paper'){
-        return `Your paper beats computers rock`;
-    }else if(computerChoice === 'rock' && userChoice === 'scissors'){
-        return `Your scissors lose against computers rock`
-    }else if(computerChoice === 'paper' && userChoice === 'scissors'){
-        return `Your scissors beats computers paper`;
-    }else if(computerChoice === 'paper' && userChoice === 'rock'){
-        return `Your rock lose against computers paper`
-    }else if(computerChoice === 'scissors' && userChoice === 'rock'){
-        return `Your rock beats computers scissors`;
-    }else if(computerChoice === 'scissors' && userChoice === 'paper'){
-        return `Your paper lose against computers scissors`
+    let finalResultsText = document.createElement('h3')
+
+
+    if (userChoice === computerChoice) {
+        finalResultsText.textContent = `Your ${userChoice} draws with computers ${computerChoice}.`
+        finalResults.appendChild(finalResultsText)
+    } else if (computerChoice === 'rock' && userChoice === 'paper') {
+        finalResultsText.textContent = `Your paper beats computers rock`;
+        finalResults.appendChild(finalResultsText)
+        playerScore += 1
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        finalResultsText.textContent = `Your scissors lose against computers rock`
+        finalResults.appendChild(finalResultsText)
+        CompScore += 1
+    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+        finalResultsText.textContent = `Your scissors beats computers paper`;
+        finalResults.appendChild(finalResultsText)
+        playerScore += 1
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        finalResultsText.textContent = `Your rock lose against computers paper`
+        finalResults.appendChild(finalResultsText)
+        CompScore += 1
+    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+        finalResultsText.textContent = `Your rock beats computers scissors`;
+        finalResults.appendChild(finalResultsText)
+        playerScore += 1
+    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+        finalResultsText.textContent = `Your paper lose against computers scissors`
+        finalResults.appendChild(finalResultsText)
+        CompScore += 1
+    }
+    playerScoreBox.value = `${playerScore}`
+    compScoreBox.value = `${CompScore}`
+
+    if (playerScoreBox.value == 5) {
+        alert('You reached 5 points, You win!')
+        location.reload()
+    }
+    if (compScoreBox.value == 5) {
+        alert('Computer reached 5 points, You lose!')
+        location.reload()
     }
 }
+
+playButton.addEventListener('click', playGame)
